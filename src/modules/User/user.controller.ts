80643323ae05b91832a39acb6dbf218e4963bf75
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-01-25 05:05:56 +0600

    trying to fix create token after deploying

--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -4,15 +4,16 @@ import { UserServices } from "./user.service";
 //import sendResponse from "../../Utils/sendResponse";
 
 const registerUser = catchAsync(async (req, res) => {
-    const { result, accessToken } = await UserServices.registerUserIntoDB(req.body);
-    const { _id, name, email, phone, role, address } = result;
+    const result  = await UserServices.registerUserIntoDB(req.body);
+    const { _id, name, email, phone, role, address } = result.result;
+    const token = result.accessToken
 
     res.status(httpStatus.OK).json({
         success: true,
         statusCode: httpStatus.OK,
         message: 'User registered successfully',
         data: { _id, name, email, phone, role, address },
-        token: accessToken
+        token: token
     });
 });
 
